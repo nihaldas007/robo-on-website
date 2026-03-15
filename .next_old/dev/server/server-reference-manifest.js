367324555f82fe5664d367cc55363569e9c8a8ef
nihaldas007 <nihaldas007@gmail.com>
2026-03-15 21:53:03 +0600

    Initial commit - Prepare for GitHub Pages deployment (Clean)

--- .next_old/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CT/YqtAqeU62tNCd4X5htKRlNkG3yWNbFVpQKMZazNM=\"\n}"
--- .next_old/dev/static/chunks/src_app_page_tsx_3794ddda._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_ded43ab5._.js"
-],
-    source: "dynamic"
-});
